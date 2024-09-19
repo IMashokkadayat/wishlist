88b5dd679af418041531a18085e659d5c273d30f
ashokkadayat <kadayatash98@gmail.com>
2024-09-19 17:58:18 +0530

    processing

---
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.shoppinglistapp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppingListAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ){
+                   ShoppingListApp()
+                }
+            }
+        }
+    }
+}
+
+
